21fcc5745db8a1cfff535896219c265d0b4700b9
Maedeh <yazdian.oul@gmail.com>
2022-03-05 16:07:15 +0330

    add code for menu in the activity and add icon for items

--- app/src/main/java/ir/homework/netflix/MainActivity.kt
+++ app/src/main/java/ir/homework/netflix/MainActivity.kt
@@ -1,22 +1,33 @@
 package ir.homework.netflix
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuBuilder
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        forceRTLIfSupported()
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu,menu)
+        if (menu is MenuBuilder) {
+            menu.setOptionalIconsVisible(true)
+        }
         return super.onCreateOptionsMenu(menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_home -> {
@@ -36,6 +47,12 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private fun forceRTLIfSupported() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL
         }
     }
 }
